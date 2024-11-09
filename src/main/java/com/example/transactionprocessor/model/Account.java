6b7e0a2e19f8aca2caa2f8171e85841dbfaf07b0
akkaponsomjai <asomjai@palo-it.com>
2024-11-09 22:10:29 +0700

    feat: Refactor Account and Transaction models to remove unused imports and add indexing for performance and add CyberSource API key

--- src/main/java/com/example/transactionprocessor/model/Account.java
+++ src/main/java/com/example/transactionprocessor/model/Account.java
@@ -1,8 +1,6 @@
 package com.example.transactionprocessor.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.math.BigDecimal;
 import lombok.Data;
--- src/main/java/com/example/transactionprocessor/model/Transaction.java
+++ src/main/java/com/example/transactionprocessor/model/Transaction.java
@@ -4,20 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(indexes = @Index(columnList = "accountId"))
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long accountId;
     private BigDecimal amount = BigDecimal.ZERO;
+    private String paymentTransactionId;
     private String status; // PENDING, AUTHORIZED, SETTLED
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
