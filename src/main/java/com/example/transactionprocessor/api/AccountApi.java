225ef171df037888af94f1716901ecd32ba2c6b3
akkaponsomjai <asomjai@palo-it.com>
2024-11-11 09:00:09 +0700

    feat: Enhance API documentation with Swagger annotations for Account and Payment APIs

--- src/main/java/com/example/transactionprocessor/api/AccountApi.java
+++ src/main/java/com/example/transactionprocessor/api/AccountApi.java
@@ -4,6 +4,7 @@ import com.example.transactionprocessor.model.Account;
 
 import com.example.transactionprocessor.model.Transaction;
 import com.example.transactionprocessor.runtime.error.exception.InvalidInputError;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -13,22 +14,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 
 public interface AccountApi {
+
+    @Operation(summary = "Get account data by account id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Results are ok"),
             @ApiResponse(responseCode = "400", description = "Invalid request", content = @Content),
-            @ApiResponse(responseCode = "404", description = "resource not found", content = @Content)})
+            @ApiResponse(responseCode = "404", description = "Resource not found", content = @Content)})
     ResponseEntity<Account> getAccountData(@PathVariable("accountId") Long accountId) throws InvalidInputError;
 
+    @Operation(summary = "Create account data by account id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Results are ok"),
             @ApiResponse(responseCode = "400", description = "Invalid request", content = @Content),
-            @ApiResponse(responseCode = "404", description = "resource not found", content = @Content)})
+            @ApiResponse(responseCode = "404", description = "Resource not found", content = @Content)})
     ResponseEntity<Account> createAccountData(@PathVariable("accountId") Long accountId) throws InvalidInputError;
 
+    @Operation(summary = "Get transactions by account id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Results are ok"),
             @ApiResponse(responseCode = "400", description = "Invalid request", content = @Content),
-            @ApiResponse(responseCode = "404", description = "resource not found", content = @Content)})
+            @ApiResponse(responseCode = "404", description = "Resource not found", content = @Content)})
     ResponseEntity<List<Transaction>> getTransactions(@PathVariable("accountId") Long accountId)
             throws InvalidInputError;
 
--- src/main/java/com/example/transactionprocessor/api/PaymentApi.java
+++ src/main/java/com/example/transactionprocessor/api/PaymentApi.java
@@ -4,6 +4,7 @@ import com.example.transactionprocessor.dto.request.ProcessPaymentRequestDTO;
 import com.example.transactionprocessor.model.Transaction;
 import com.example.transactionprocessor.runtime.error.exception.CyberSourceError;
 import com.example.transactionprocessor.runtime.error.exception.InvalidInputError;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -15,10 +16,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 public interface PaymentApi {
 
+    @Operation(summary = "Process payment with CyberSource SDK.",
+            description = "Please call PUT /api/accounts/{accountId} to create an account before this api." +
+            "After procceeding the payment, you can check the account balance through GET /api/accounts/{accountId}.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Results are ok"),
             @ApiResponse(responseCode = "400", description = "Invalid request", content = @Content),
-            @ApiResponse(responseCode = "404", description = "resource not found", content = @Content)})
+            @ApiResponse(responseCode = "404", description = "Resource not found", content = @Content)})
     ResponseEntity<Transaction> processPayment(ProcessPaymentRequestDTO processPaymentRequestDTO)
             throws InvalidInputError, CyberSourceError;
 
